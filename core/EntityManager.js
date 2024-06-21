0df1dd1b4575b1a45ba2462d87e1a12e6a4182a0
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-06-20 21:27:52 -0700

    Update EntityManager.js

--- core/EntityManager.js
+++ core/EntityManager.js
@@ -5,11 +5,12 @@ import { Bird } from '../entities/Bird.js';
 import { Worm } from '../entities/Worm.js';
 import { Bush } from '../entities/Bush.js';
 import { Tree } from '../entities/Tree.js';
-import { ButterflyPool } from '../entities/ButterflyPool.js';
 import { GameConfig } from '../config/GameConfig.js';
+import { MathUtils } from '../utils/MathUtils.js';
 
 export class EntityManager {
-    constructor() {
+    constructor(game) {
+        this.game = game;
         this.entities = {
             butterflies: [],
             birds: [],
@@ -17,55 +18,50 @@ export class EntityManager {
             bushes: [],
             trees: []
         };
-        this.butterflyPool = new ButterflyPool();
-    }
-
-    initialize() {
-        // Any initialization logic for EntityManager
     }
 
     addBush(x, y) {
         const bush = new Bush(x, y);
         this.entities.bushes.push(bush);
-        this.addButterflies(bush);
+        this.game.renderer.addToScene(bush.element);
+        this.spawnButterfliesForBush(bush);
     }
 
     addTree(x, y) {
         const tree = new Tree(x, y);
         this.entities.trees.push(tree);
+        this.game.renderer.addToScene(tree.element);
     }
 
     addBird(x, y) {
         const bird = new Bird(x, y);
         this.entities.birds.push(bird);
+        this.game.renderer.addToScene(bird.element);
     }
 
     addWorm(x, y) {
         const worm = new Worm(x, y);
         this.entities.worms.push(worm);
+        this.game.renderer.addToScene(worm.element);
     }
 
-    addButterflies(bush) {
-        const numButterflies = Math.floor(Math.random() * 2) + 1;
+    spawnButterfliesForBush(bush) {
+        const numButterflies = Math.floor(Math.random() * 2) + 1; // 1-2 butterflies
         for (let i = 0; i < numButterflies; i++) {
-            const butterflyElement = this.butterflyPool.get();
-            const butterfly = new Butterfly(butterflyElement, bush);
+            const butterflyX = bush.x + (Math.random() - 0.5) * 100;
+            const butterflyY = bush.y + (Math.random() - 0.5) * 100;
+            const butterfly = new Butterfly(butterflyX, butterflyY, bush);
             this.entities.butterflies.push(butterfly);
+            this.game.renderer.addToScene(butterfly.element);
         }
     }
 
-    updateEntities(currentTime) {
-        this.updateEntityType(this.entities.butterflies, currentTime);
-        this.updateEntityType(this.entities.birds, currentTime);
-        this.updateEntityType(this.entities.worms, currentTime);
-        this.updateEntityType(this.entities.bushes, currentTime);
-        this.updateEntityType(this.entities.trees, currentTime);
+    updateEntities(deltaTime) {
+        Object.values(this.entities).forEach(entityGroup => {
+            entityGroup.forEach(entity => entity.update(deltaTime));
+        });
 
         this.handleInteractions();
-    }
-
-    updateEntityType(entities, currentTime) {
-        entities.forEach(entity => entity.update(currentTime));
     }
 
     handleInteractions() {
@@ -75,82 +71,49 @@ export class EntityManager {
 
     handleButterflyPollination() {
         this.entities.butterflies.forEach(butterfly => {
-            if (butterfly.carriesPollen) {
-                const nearbyBush = this.findNearbyBush(butterfly);
-                if (nearbyBush && nearbyBush !== butterfly.pollenSource) {
-                    if (Math.random() < GameConfig.POLLINATION_CHANCE) {
-                        this.createNewFlowerBush(nearbyBush);
-                    }
-                    butterfly.carriesPollen = false;
-                    butterfly.pollenSource = null;
-                }
-            } else {
-                const nearbyBush = this.findNearbyBush(butterfly);
-                if (nearbyBush) {
-                    butterfly.carriesPollen = true;
-                    butterfly.pollenSource = nearbyBush;
-                }
+            const nearbyBush = this.findNearestEntity(butterfly, this.entities.bushes, GameConfig.BUTTERFLY_POLLINATION_RANGE);
+            if (nearbyBush) {
+                butterfly.pollinate(nearbyBush);
             }
         });
     }
 
     handleBirdHunting() {
         this.entities.birds.forEach(bird => {
-            if (bird.isHunting) {
-                const nearbyWorm = this.findNearbyWorm(bird);
+            if (bird.isHunting()) {
+                const nearbyWorm = this.findNearestEntity(bird, this.entities.worms, GameConfig.BIRD_HUNTING_RANGE);
                 if (nearbyWorm) {
                     bird.hunt(nearbyWorm);
-                    this.removeWorm(nearbyWorm);
+                    this.removeEntity(nearbyWorm);
                 }
             }
         });
     }
 
-    findNearbyBush(butterfly) {
-        return this.entities.bushes.find(bush => 
-            this.getDistance(butterfly.getPosition(), bush.getPosition()) < GameConfig.BUTTERFLY_DETECTION_RADIUS
-        );
+    findNearestEntity(entity, entityGroup, maxDistance) {
+        return entityGroup.reduce((nearest, current) => {
+            const distance = MathUtils.getDistance(entity, current);
+            if (distance < maxDistance && (!nearest || distance < MathUtils.getDistance(entity, nearest))) {
+                return current;
+            }
+            return nearest;
+        }, null);
     }
 
-    findNearbyWorm(bird) {
-        return this.entities.worms.find(worm => 
-            this.getDistance(bird.getPosition(), worm.getPosition()) < GameConfig.BIRD_HUNTING_RADIUS
-        );
-    }
-
-    createNewFlowerBush(nearBush) {
-        const bushPosition = nearBush.getPosition();
-        const angle = Math.random() * 2 * Math.PI;
-        const distance = Math.random() * 100 + 50; // 50-150px away
-        const newPosition = {
-            x: bushPosition.x + distance * Math.cos(angle),
-            y: bushPosition.y + distance * Math.sin(angle)
-        };
-        this.addBush(newPosition.x, newPosition.y);
-    }
-
-    removeWorm(worm) {
-        const index = this.entities.worms.indexOf(worm);
-        if (index > -1) {
-            this.entities.worms.splice(index, 1);
+    removeEntity(entity) {
+        for (let groupName in this.entities) {
+            const index = this.entities[groupName].indexOf(entity);
+            if (index !== -1) {
+                this.entities[groupName].splice(index, 1);
+                this.game.renderer.removeFromScene(entity.element);
+                break;
+            }
         }
-    }
-
-    getDistance(pos1, pos2) {
-        const dx = pos1.x - pos2.x;
-        const dy = pos1.y - pos2.y;
-        return Math.sqrt(dx*dx + dy*dy);
-    }
-
-    renderEntities(renderer) {
-        Object.values(this.entities).flat().forEach(entity => {
-            entity.render(renderer);
-        });
     }
 
     getEntitiesInRadius(position, radius) {
         return Object.values(this.entities).flat().filter(entity => 
-            this.getDistance(position, entity.getPosition()) <= radius
+            MathUtils.getDistance(position, entity) <= radius
         );
     }
 }
