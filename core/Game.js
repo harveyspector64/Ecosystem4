fbca7b044dab2fd8f3e72a67a82877875f1f210c
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-06-20 22:36:39 -0700

    Update Game.js

--- core/Game.js
+++ core/Game.js
@@ -1,19 +1,19 @@
 // src/core/Game.js
 
+import { UIManager } from '../ui/UIManager.js';
+import { Renderer } from '../rendering/Renderer.js';
 import { EntityManager } from './EntityManager.js';
 import { EventSystem } from './EventSystem.js';
 import { StateManager } from './StateManager.js';
-import { UIManager } from '../ui/UIManager.js';
-import { Renderer } from '../rendering/Renderer.js';
 import { GameConfig } from '../config/GameConfig.js';
 
 export class Game {
     constructor() {
+        this.uiManager = new UIManager(this);
+        this.renderer = new Renderer();
         this.entityManager = new EntityManager(this);
         this.eventSystem = new EventSystem();
         this.stateManager = new StateManager();
-        this.uiManager = new UIManager(this);
-        this.renderer = new Renderer();
 
         this.isRunning = false;
         this.lastUpdateTime = 0;
@@ -22,20 +22,19 @@ export class Game {
     }
 
     initialize() {
-        this.uiManager.initialize();
-        this.stateManager.initialize();
-        this.entityManager.initialize();
         this.renderer.initialize(document.getElementById('play-area'));
+        this.uiManager.initialize();
+        this.entityManager.initialize();
+        this.stateManager.initialize();
 
         // Set up event listeners
-        this.eventSystem.subscribe('emojiAdded', this.handleEmojiAdded.bind(this));
-        this.eventSystem.subscribe('butterflyDied', this.handleButterflyDied.bind(this));
-        this.eventSystem.subscribe('birdDied', this.handleBirdDied.bind(this));
-        this.eventSystem.subscribe('createNewBush', this.handleCreateNewBush.bind(this));
-        
+        this.eventSystem.subscribe('emojiDropped', this.handleEmojiDrop.bind(this));
+        this.eventSystem.subscribe('entityCreated', this.handleEntityCreated.bind(this));
+        this.eventSystem.subscribe('entityRemoved', this.handleEntityRemoved.bind(this));
+
         this.isRunning = true;
         this.lastUpdateTime = performance.now();
-        this.gameLoop(this.lastUpdateTime);
+        this.gameLoop();
     }
 
     gameLoop(currentTime) {
@@ -54,13 +53,17 @@ export class Game {
         this.updateDayNightCycle(deltaTime);
         this.entityManager.updateEntities(deltaTime);
         this.stateManager.update(deltaTime);
-        this.updateEntityCounts();
+    }
+
+    render() {
+        this.renderer.clear();
+        this.entityManager.renderEntities(this.renderer);
     }
 
     updateDayNightCycle(deltaTime) {
-        this.cycleTimer += deltaTime * 1000; // Convert back to milliseconds
-
+        this.cycleTimer += deltaTime;
         const totalCycleTime = GameConfig.DAY_DURATION + GameConfig.NIGHT_DURATION;
+        
         if (this.cycleTimer >= totalCycleTime) {
             this.cycleTimer -= totalCycleTime;
         }
@@ -69,91 +72,79 @@ export class Game {
         
         if (newCycle !== this.dayNightCycle) {
             this.dayNightCycle = newCycle;
-            this.eventSystem.publish('dayNightChange', this.dayNightCycle);
             this.handleDayNightChange();
         }
     }
 
     handleDayNightChange() {
-        if (this.dayNightCycle === 'night') {
-            this.entityManager.hideAllButterflies();
-            this.renderer.applyDayNightEffect(true);
-            // TODO: Spawn nocturnal animals
-        } else {
-            this.entityManager.showAllButterflies();
-            this.renderer.applyDayNightEffect(false);
-            // TODO: Remove nocturnal animals
-        }
+        this.eventSystem.publish('dayNightChange', this.dayNightCycle);
+        this.renderer.applyDayNightEffect(this.dayNightCycle === 'night');
+        this.entityManager.handleDayNightChange(this.dayNightCycle);
         this.uiManager.updateDayNightCycle(this.dayNightCycle);
     }
 
-    handleEmojiAdded(data) {
+    handleEmojiDrop(data) {
         const { emoji, x, y } = data;
         switch(emoji) {
             case GameConfig.EMOJIS.BUSH:
                 if (this.stateManager.canPlantBush()) {
                     this.entityManager.addBush(x, y);
                     this.stateManager.incrementBushCount();
-                    this.uiManager.unlockTree();
+                    this.uiManager.addNewsItem("A new bush has been planted!");
                 }
                 break;
             case GameConfig.EMOJIS.TREE:
                 if (this.stateManager.canPlantTree()) {
                     this.entityManager.addTree(x, y);
                     this.stateManager.incrementTreeCount();
-                    this.entityManager.addBird(x, y);
+                    this.uiManager.addNewsItem("A new tree has been planted!");
                 }
                 break;
             case GameConfig.EMOJIS.WORM:
                 if (this.entityManager.getWormCount() < GameConfig.MAX_WORMS) {
                     this.entityManager.addWorm(x, y);
+                    this.uiManager.addNewsItem("A worm has been added to the ecosystem!");
                 }
                 break;
         }
-        this.eventSystem.gameEvent('logEvent', `A ${this.getEmojiName(emoji)} has been added to the ecosystem!`);
     }
 
-    handleButterflyDied(data) {
-        this.eventSystem.gameEvent('logEvent', `A butterfly has died.`);
+    handleEntityCreated(data) {
+        const { type, id } = data;
+        this.uiManager.addNewsItem(`A new ${type} (ID: ${id}) has joined the ecosystem!`);
     }
 
-    handleBirdDied(data) {
-        this.eventSystem.gameEvent('logEvent', `A bird has died.`);
+    handleEntityRemoved(data) {
+        const { type, id, reason } = data;
+        this.uiManager.addNewsItem(`${type} (ID: ${id}) has left the ecosystem. Reason: ${reason}`);
     }
 
-    handleCreateNewBush(data) {
-        const { nearBush } = data;
-        const angle = Math.random() * Math.PI * 2;
-        const distance = Math.random() * 100 + 50; // 50-150px away
-        const newX = nearBush.x + distance * Math.cos(angle);
-        const newY = nearBush.y + distance * Math.sin(angle);
-        
-        if (this.stateManager.canPlantBush()) {
-            this.entityManager.addBush(newX, newY);
-            this.stateManager.incrementBushCount();
-            this.eventSystem.gameEvent('logEvent', `A new bush has grown from pollination!`);
+    unlockTree() {
+        this.stateManager.unlockTree();
+        this.uiManager.unlockTree();
+    }
+
+    addWormToPanel() {
+        if (this.stateManager.shouldUnlockWorm()) {
+            this.uiManager.addWormToPanel();
+            this.stateManager.unlockWorm();
         }
     }
 
-    getEmojiName(emoji) {
-        for (let key in GameConfig.EMOJIS) {
-            if (GameConfig.EMOJIS[key] === emoji) return key.toLowerCase();
+    checkWinCondition() {
+        if (this.stateManager.checkWinCondition()) {
+            this.isRunning = false;
+            this.uiManager.showWinScreen();
         }
-        return 'unknown';
     }
 
-    updateEntityCounts() {
-        const counts = {
-            butterflies: this.entityManager.entities.butterflies.length,
-            birds: this.entityManager.entities.birds.length,
-            worms: this.entityManager.entities.worms.length,
-            bushes: this.entityManager.entities.bushes.length,
-            trees: this.entityManager.entities.trees.length
-        };
-        this.uiManager.updateEntityCounts(counts);
-    }
-
-    render() {
-        this.entityManager.renderEntities(this.renderer);
+    reset() {
+        this.entityManager.reset();
+        this.stateManager.reset();
+        this.uiManager.reset();
+        this.cycleTimer = 0;
+        this.dayNightCycle = 'day';
+        this.isRunning = true;
+        this.gameLoop();
     }
 }
